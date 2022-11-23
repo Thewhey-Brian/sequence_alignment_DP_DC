26b1c4802e47076638033411ce8dec26abaeb426
Xinyu Guo <54336221+Thewhey-Brian@users.noreply.github.com>
2022-11-22 16:12:54 -0800

    Updated efficient version

--- efficient_3.py
+++ efficient_3.py
@@ -40,7 +40,7 @@ def generate_stings(input_file):
     strings.append(string)
     return strings
 
-def alignment(string1, string2): 
+def alignment_efficient(string1, string2): 
     """
     Function to align two strings (gap penalty and mismatch penalty are hardcoded).
 
@@ -53,51 +53,88 @@ def alignment(string1, string2):
     alpha = [0] * 26 # code alpha values in one dimention
     alpha[2], alpha[3], alpha[5], alpha[6], alpha[10], alpha[15] = 110, 48, 94, 118, 48, 110 # assign corresponding values
     delta = 30
-    dp = np.zeros((len(string1) + 1, len(string2) + 1)) # create result holder
-    for i in range(dp.shape[0]):  
-        for j in range(dp.shape[1]): 
-            if i == 0: # initialize 
-                dp[i, j] = j * delta
+    if len(string1) < 3 or len(string2) < 3: 
+        dp = np.zeros((2, 2))
+        for i in range(dp.shape[0]):  
+            for j in range(dp.shape[1]): 
+                if i == 0: # initialize 
+                    dp[i, j] = j * delta
+                    continue
+                if j == 0: # initialize 
+                    dp[i, j] = i * delta
+                    continue
+                dp[i, j] = min(dp[i - 1, j - 1] + alpha[code_dic[string1[i - 1]] * code_dic[string2[j - 1]]], # DP step
+                            dp[i - 1, j] + delta, 
+                            dp[i, j - 1] + delta)
+        cost = int(dp[i, j])
+        aligned1_back = ''
+        aligned2_back = '' 
+        while (i != 0) or (j != 0):
+            if i == 0: # when string2 is longer
+                aligned1_back += '_'
+                aligned2_back += string2[j - 1]
+                j -= 1
                 continue
-            if j == 0: # initialize 
-                dp[i, j] = i * delta
+            if j == 0: # when string1 is longer
+                aligned1_back += string1[i - 1]
+                aligned2_back += '_'
+                i -= 1
                 continue
-            dp[i, j] = min(dp[i - 1, j - 1] + alpha[code_dic[string1[i - 1]] * code_dic[string2[j - 1]]], # DP step
-                        dp[i - 1, j] + delta, 
-                        dp[i, j - 1] + delta)
-    cost = int(dp[i, j]) # get the min cost for the alignment
-    aligned1_back = ''
-    aligned2_back = ''    
-    while (i != 0) or (j != 0):
-        if i == 0: # when string2 is longer
-            aligned1_back += '_'
-            aligned2_back += string2[j - 1]
-            j -= 1
-            continue
-        if j == 0: # when string1 is longer
-            aligned1_back += string1[i - 1]
-            aligned2_back += '_'
-            i -= 1
-            continue
-        diag_value = dp[i - 1, j - 1] + alpha[code_dic[string1[i - 1]] * code_dic[string2[j - 1]]] # traceback diagonal
-        if diag_value == dp[i, j]: 
-            aligned1_back += string1[i - 1]
-            aligned2_back += string2[j - 1]
-            i -= 1
-            j -= 1
-            continue
-        if dp[i - 1, j] + delta == dp[i, j]: # traceback along string1
-            aligned1_back += string1[i - 1]
-            aligned2_back += '_'
-            i -= 1
-            continue
-        if dp[i, j - 1] + delta == dp[i, j]: #traceback along string2
-            aligned1_back += "_"
-            aligned2_back += string2[j - 1]
-            j -= 1
-    aligned1 = aligned1_back[::-1] # get aligned string1
-    aligned2 = aligned2_back[::-1] # get aligned string2
-    return cost, aligned1, aligned2
+            diag_value = dp[i - 1, j - 1] + alpha[code_dic[string1[i - 1]] * code_dic[string2[j - 1]]] # traceback diagonal
+            if diag_value == dp[i, j]: 
+                aligned1_back += string1[i - 1]
+                aligned2_back += string2[j - 1]
+                i -= 1
+                j -= 1
+                continue
+            if dp[i - 1, j] + delta == dp[i, j]: # traceback along string1
+                aligned1_back += string1[i - 1]
+                aligned2_back += '_'
+                i -= 1
+                continue
+            if dp[i, j - 1] + delta == dp[i, j]: #traceback along string2
+                aligned1_back += "_"
+                aligned2_back += string2[j - 1]
+                j -= 1
+        aligned1 = aligned1_back[::-1] # get aligned string1
+        aligned2 = aligned2_back[::-1] # get aligned string2
+        return cost, aligned1, aligned2
+    dp_l = np.zeros((2, len(string2) + 1))
+    dp_r = np.zeros((2, len(string2) + 1))
+    for i in range(len(string1) // 2 + 1): 
+        for j in range(dp_l.shape[1]): 
+            if i == 0:
+                dp_l[i, j] = j * delta
+                continue
+            if j == 0:
+                dp_l[1, 0] = i * delta
+                continue
+            dp_l[1, j] = min(dp_l[0, j - 1] + alpha[code_dic[string1[i - 1]] * code_dic[string2[j - 1]]], 
+                            dp_l[0, j] + delta, 
+                            dp_l[1, j - 1] + delta)
+        for j in range(dp_l.shape[1]): 
+            dp_l[0, j] = dp_l[1, j]
+    for i in range(len(string1) - (len(string1) // 2) + 1): 
+        for j in range(dp_r.shape[1]): 
+            if i == 0: 
+                dp_r[i, j] = j * delta
+                continue
+            if j == 0: 
+                dp_r[1, 0] = i * delta
+                continue
+            dp_r[1, j] = min(dp_r[0, j - 1] + alpha[code_dic[string1[len(string1) - i]] * code_dic[string2[len(string2) - j]]], 
+                            dp_r[0, j] + delta, 
+                            dp_r[1, j - 1] + delta)
+            for j in range(dp_l.shape[1]): 
+                dp_r[0, j] = dp_r[1, j]
+    cut_point = 0
+    for j in range(dp_r.shape[1]): 
+        if dp_l[0, j] + dp_r[0, len(string2) - j] < dp_l[0, cut_point] + dp_r[0, len(string2) - cut_point]: 
+            cut_point = j
+    cost = dp_l[0, cut_point] + dp_r[0, len(string2) - cut_point]
+    aligned1_l, aligned2_l = alignment_efficient(string1[:(len(string1) // 2)], string2[:cut_point])
+    aligned1_r, aligned2_r = alignment_efficient(string1[(len(string1) // 2):], string2[cut_point:])
+    return cost, aligned1_l + aligned1_r, aligned2_l + aligned2_r
 
 def process_memory():
     """
